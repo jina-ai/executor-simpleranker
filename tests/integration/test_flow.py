faed32490b307af6524b1f7980d9724837b060e3
cristian m <cristian.mitroi@jina.ai>
2022-03-09 15:19:02 +0100

    fix: namedscore

--- tests/integration/test_flow.py
+++ tests/integration/test_flow.py
@@ -9,13 +9,12 @@ def test_integration(documents_chunk):
     with Flow().add(uses=SimpleRanker, uses_with={'metric': 'cosine'}) as flow:
         resp = flow.post(on='/search', inputs=documents_chunk, return_results=True)
 
-    for r in resp:
-        for doc in r.docs:
-            assert doc.matches
-            for i in range(len(doc.matches) - 1):
-                match = doc.matches[i]
-                assert match.tags
-                assert (
-                    match.scores['cosine']
-                    <= doc.matches[i + 1].scores['cosine']
-                )
+    for doc in resp:
+        assert doc.matches
+        for i in range(len(doc.matches) - 1):
+            match = doc.matches[i]
+            assert match.tags
+            assert (
+                match.scores['cosine'].value
+                <= doc.matches[i + 1].scores['cosine'].value
+            )
